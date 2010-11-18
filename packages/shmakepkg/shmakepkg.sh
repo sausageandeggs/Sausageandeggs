9595207d8cdd75193ec6f59c6234b2e1e9e9fd17
sausageandeggs <sausageandeggs@archlinux.us>
2010-11-18 22:54:56 +0000

    shmakepkg changes

--- packages/shmakepkg/shmakepkg.sh
+++ packages/shmakepkg/shmakepkg.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# shmakepkg	{{{
-# Ver 2
-# Links $srcdir to /dev/shm
+# shmakepkg	{{{ 
+# Ver 3
+# Links $srcdir to /dev/shm or removes current link to /dev/shm
+# and then calls makepkg with specified  options
 # Usage: shmakepkg [usual makepkg options] }}}
 
 red='\e[1;31m' # Red - bold {{{
@@ -14,26 +15,47 @@ pur='\e[1;35m' # Purple
 cyn='\e[1;36m' # Cyan
 bld='\e[0m\033[1m' # plain bold
 rst='\e[0m' # Txt reset }}}
-srcdir=src # 																{{{
+
+srcdir=src # {{{
 mkopts=$@
 makepkg="makepkg $mkopts"
-devshm=/dev/shm/$USER/makepkg/$pkgname # 									}}}
+devshm=/dev/shm/$USER/makepkg/$pkgname
+. PKGBUILD # }}}
 
-. PKGBUILD
-
-if [[ -e $srcdir && $(readlink $srcdir) != $devshm ]]; then
-	echo -e "${grn}==>${red} Error, ${ylw}$srcdir${bld} exists and doesn't link to ${pur}$devshm${bld}, aborting${rst}"; return 1
-elif [[ -f $srcdir/.tmpfs ]]; then
-	echo -en "${grn}==>${bld} Delete ${ylw}$srcdir${bld} and ${pur}$devshm${grn} (1)${bld} or Build ${cyn}$pkgname ${grn}(2)${rst}:"
-	read -n 1 ans
-	echo
-	[[ $ans == 1 ]] && rm -rf $devshm $srcdir && \
-		echo -e "${grn}==>${bld} Success, Removed ${ylw}$srcdir and ${pur}$devshm${rst}"
-	[[ $ans == 2 ]] && $makepkg
-else
+shmake() { # {{{ 
 	install -dm 700 $devshm
 	ln -s $devshm $srcdir
 	touch $srcdir/.tmpfs
-	echo -e "${grn}==>${bld} Success, ${ylw}$srcdir linked to ${pur}$devshm${rst}"
-	$makepkg
+	echo -e "${grn}==> ${ylw}$srcdir now links to ${pur}$devshm${rst}"
+} # }}}
+
+if [[ $@ == "-n" ]]; then 
+	if [[ -e $srcdir && $(readlink $srcdir) != $devshm ]]; then
+		echo -e "${grn}==>${red} Error, ${ylw}$srcdir${bld} exists but doesn't link to ${pur}$devshm${bld}"
+		echo -en "${grn}==>${bld} Hit (y) to remove ${ylw}$srcdir${bld}, recreate, link to ${pur}$devshm${bld}.\n    Any other key to abort: "
+		read -n 1 ans
+		echo
+		[[ $ans == y ]] && rm -rf $devshm $srcdir && shmake|| return 1
+	elif [[ -e $srcdir && $(readlink $srcdir) == $devshm ]]; then
+		echo -e "${grn}==> ${ylw}$srcdir${bld} already links to ${pur}$devshm${bld} :"
+	else
+		shmake
+	fi
+else
+	if [[ -e $srcdir && $(readlink $srcdir) != $devshm ]]; then
+		echo -e "${grn}==>${red} Error, ${ylw}$srcdir${bld} exists but doesn't link to ${pur}$devshm${bld}"
+		echo -en "${grn}==>${bld} Hit (y) to remove ${ylw}$srcdir${bld}, recreate, link to ${pur}$devshm${bld} and continue build.\n    Any other key to abort: "
+		read -n 1 ans
+		echo
+		[[ $ans == y ]] && rm -rf $devshm $srcdir && shmake && $makepkg|| return 1
+	elif [[ -f $srcdir/.tmpfs ]]; then
+		echo -en "${grn}==>${bld} Do you want to${grn} (1) ${bld}Delete ${ylw}$srcdir${bld} and ${pur}$devshm${bld} or${grn} (2) ${bld}Build ${cyn}$pkgname :"
+		read -n 1 ans
+		echo
+		[[ $ans == 1 ]] && rm -rf $devshm $srcdir && \
+			echo -e "${grn}==>${bld} Removed ${ylw}$srcdir and ${pur}$devshm${rst}"
+		[[ $ans == 2 ]] && $makepkg
+	else
+		shmake && $makepkg
+	fi
 fi
