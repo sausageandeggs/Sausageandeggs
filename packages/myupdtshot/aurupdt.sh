3e4d8a733f22103745c7c3a0b26b73d6c6ee23eb
sausageandeggs <sausageandeggs@archlinux.us>
2010-11-19 13:48:33 +0000

    Added up2date line

--- packages/myupdtshot/aurupdt.sh
+++ packages/myupdtshot/aurupdt.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 updtlist=/tmp/aurupdis.list
-#bauerbill -Qu --aur > $updtlist
 
-cat $updtlist | grep -v ^$ | grep AUR | cut -d/ -f2 | awk '{print "\${goto 50}\${color2} " $1 " \${color1}\$alignr "$2 "\${color0} ==> \${color1}" $4 } ' 2>/dev/null
+if [[ $(wc -l < $updtlist) == 0 ]]; then
+	echo "${goto 50}${font Arial:bold:size=10}${color 9BC58B}    You are up to date"
+else
+	cat $updtlist | grep -v ^$ | grep AUR | cut -d/ -f2 | awk '{print "\${goto 50}\${color2} " $1 " \${color1}\$alignr "$2 "\${color0} ==> \${color1}" $4 } ' 2>/dev/null
+fi
 
