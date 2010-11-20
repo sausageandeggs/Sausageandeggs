5be70ec702a73725aa0b1e8642c96b5bd2e40a6a
sausageandeggs <sausageandeggs@archlinux.us>
2010-11-20 02:22:44 +0000

    Bug

--- packages/myupdtshot/aurupdt.sh
+++ packages/myupdtshot/aurupdt.sh
@@ -3,7 +3,7 @@
 updtlist=/tmp/aurupdis.list
 
 if [[ $(wc -l < $updtlist) == 0 ]]; then
-	echo "${goto 50}${font Arial:bold:size=10}${color 9BC58B}    You are up to date"
+	echo "\${goto 50}\${font Arial:bold:size=10}\${color 9BC58B}    You are up to date"
 else
 	cat $updtlist | grep -v ^$ | grep AUR | cut -d/ -f2 | awk '{print "\${goto 50}\${color2} " $1 " \${color1}\$alignr "$2 "\${color0} ==> \${color1}" $4 } ' 2>/dev/null
 fi
