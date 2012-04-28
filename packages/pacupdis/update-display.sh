71d8a27f29e5a609adea7d367dc86deecaf986c3
sausageandeggs <sausageandeggs@archlinux.us>
2012-04-28 02:49:43 +0100

    pacupdis Reduce to a single expac call

--- packages/pacupdis/update-display.sh
+++ packages/pacupdis/update-display.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-### pacupdis Ver 0.94 ###
-###### Config bit #########################################################################
-left_font="\${goto 59}\${font liberation:bold:size=8}"  # Font for Pkg name and version
-right_font=""                                           # Font for Pkg size
-right_color="\${color1}"                                # Colour for Pkg size
-core_color="\${color DF938F}"                           # Colour for core pkgs
-extra_color="\${color0}"                                # Colour for extra pkgs
-community_color="\${color CF9ECC}"                      # Colour for community pkgs
-multilib_color="\${color 8FDF99}"                       # Colour for multilib pkgs
-kerne26_ck_color="\${color C55154}"                     # Colour for multilib pkgs
-xyne_color="\${color D3D181}"                           # Colour for xyne-any pkgs
-seperator="\${color darkslategray}\${hr 2}"             # Between updates and summary
-sumline="\${goto 59}\${color2}\${font liberation:bold:size=8}" # Totals etc
-###########################################################################################
+### pacupdis Ver 0.95 ###
+###### Config bit ######################################################################### {{{
+left_font="\${goto 59}\${font liberation:bold:size=8}"  ## Font for Pkg name and version ##
+right_font=""                                           ## Font for Pkg size 		 	 ##
+right_color="\${color1}"                                ## Colour for Pkg size			 ##
+core_color="\${color DF938F}"                           ## Colour for core pkgs 		 ##
+extra_color="\${color0}"                                ## Colour for extra pkgs 	 	 ##
+community_color="\${color CF9ECC}"                      ## Colour for community pkgs  	 ##
+multilib_color="\${color 8FDF99}"                       ## Colour for multilib pkgs      ##
+kernel26_ck_color="\${color C55154}"                    ## Colour for kerne26_ckpkgs     ##
+xyne_color="\${color D3D181}"                           ## Colour for xyne-any pkgs   	 ##
+seperator="\${color darkslategray}\${hr 2}"             ## Between updates and summary   ##
+sumline="\${goto 59}\${color2}\${font liberation:bold:size=8}" ## Totals etc 			 ##
+########################################################################################### }}}
 
 list_updates() { #{{{
-    declare -a colour
-    local let a=0 b=0 totalmb=0 ### boo
-    for j in "${updts[*]}";do
-        declare -a newver[a]="$(expac '%n-%v' -S "$j" | grep -m 1 "$j")"
-        declare -a repo[a]="$(expac $'%r' -S "$j" | grep -m 1 [a-z])"
-        declare -a size[a]="$(expac '%k' -S "$j" | grep -m 1 [0-9] | awk '{print $1 / 1024}')"
-        declare -a totsize[a]="$(expac '%k' -S "$j" | awk '{print $1}')"
+    declare -a all=() colour=() size=() totsize=()
+    local let a=0 b=0 totalmb=0
+    for j in ${updts[*]};do
+        declare -a all[a]=$(expac -H M '%n-%v %r %m' -S $j)
+        declare -a newver[a]="$(echo ${all[a]} | awk '{print $1}')"
+        declare -a repo[a]="$(echo ${all[a]} | awk '{print $2}')"
+        declare -a size[a]="$(echo ${all[a]} | awk '{print $3}')"
+        declare -a totsize[a]=${size[a]}
         totalmb=$(echo ${size[a]} | awk '{print $1 + '$totalmb'}')
         (( a++ ))
     done
 
-    for k in "${updts[*]}";do
-        case "${repo[b]}" in
+    for k in ${updts[*]};do
+        case ${repo[b]} in
         core)
         colour[b]="$core_color" ;;
         extra)
@@ -40,27 +41,27 @@ list_updates() { #{{{
         xyne-any)
         colour[b]=$xyne_color ;;
         kernel26-ck)
-        colour[b]=$kerne26_ck_color ;;
+        colour[b]=$kernel26_ck_color ;;
         esac
-        printf "%b %.3f %b\n" "\${goto 59}${left_font}${colour[b]}${newver[b]}${right_color}${right_font}\${alignr}" "${size[b]}" "Mb"
+        printf "%b %.2f %b\n" "${left_font}${colour[b]}${newver[b]}${right_color}${right_font}\${alignr}" "${size[b]}" "Mb"
         (( b++ ))
     done
     echo "\${goto 59}${seperator}"
-    if [[ "${#updts[*]}" == "1" ]]; then
-       printf "%b %.3f %b\n" "${sumline}Total: "${#updts[*]}" update available\${alignr}" "${totalmb}" "Mb"
+    if [[ ${#updts[*]} == "1" ]]; then
+       printf "%b %.2f %b\n" "${sumline}Total: ${#updts[*]} update available\${alignr}" "${totalmb}" "Mb"
    else
-       printf "%b %.3f %b\n" "${sumline}Total: "${#updts[*]}" updates available\${alignr}" "${totalmb}" "Mb"
+       printf "%b %.2f %b\n" "${sumline}Total: ${#updts[*]} updates available\${alignr}" "${totalmb}" "Mb"
    fi
 } #}}}
 
 declare -a updts=($(sudo pacman -Qqu))
 declare -a ignpkgs=($(sed -n "/IgnorePkg/s/^\s*IgnorePkg\s*=\([^#]*\).*$/\1/p" /etc/pacman.conf))
 
-for i in "${ignpkgs[@]}";do
-    updts=("${updts[@]##$i}")
+for i in ${ignpkgs[*]};do
+    updts=(${updts[*]/$i/})
 done
 
-if [[ "${#updts[*]}" == "0" ]]; then
+if [[ ${#updts[*]} == "0" ]]; then
     echo "\${voffset 10}\${alignc}\${font Arial:bold:size=10}\${color1}You are up to date"
 else
     list_updates
